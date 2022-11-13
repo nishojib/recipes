40af7ac9168eed5d81b81d9b11833ff12880b999
Nur Islam <nishojib@gmail.com>
2022-11-13 17:27:00 -0500

    added recipes sqlite database and added recipes endpoint with findall query

--- server/src/main/java/nishojib/App.java
+++ server/src/main/java/nishojib/App.java
@@ -6,6 +6,7 @@ import com.google.gson.GsonBuilder;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import static spark.Spark.get;
 
@@ -15,11 +16,45 @@ public class App {
         builder.setPrettyPrinting();
         Gson gson = builder.create();
 
+        get("recipes", (req, res) -> {
+            res.type("application/json");
+
+            Connect connection = new Connect();
+            Connection conn = connection.connect("recipes.sqlite");
+
+            String sql = "SELECT * FROM recipes";
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+            ArrayList<Recipe> recipes = new ArrayList<>();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String image = rs.getString("image");
+                int servings = rs.getInt("servings");
+                int healthScore = rs.getInt("healthScore");
+                int cheap = rs.getInt("cheap");
+                int glutenFree = rs.getInt("glutenFree");
+                int dairyFree = rs.getInt("dairyFree");
+                int readyInMinutes = rs.getInt("readyInMinutes");
+                String instructions = rs.getString("instructions");
+                String summary = rs.getString("summary");
+
+                recipes.add(new Recipe(id, title, image, servings, healthScore, cheap, glutenFree, dairyFree, readyInMinutes, instructions, summary));
+            }
+
+            conn.close();
+            return gson.toJson(recipes);
+
+        });
+
         get("tracks", (req, res) -> {
             res.type("application/json");
 
             Connect connection = new Connect();
-            Connection conn = connection.connect();
+            Connection conn = connection.connect("chinook.db");
 
             String sql = "SELECT TrackId, Name, Composer, Milliseconds, Bytes, UnitPrice FROM tracks";
 
---
+++ server/src/main/java/nishojib/Recipe.java
@@ -0,0 +1,29 @@
+package nishojib;
+
+public class Recipe {
+    private int id;
+    private String title;
+    private String image;
+    private int servings;
+    private int healthScore;
+    private boolean cheap;
+    private boolean glutenFree;
+    private boolean dairyFree;
+    private int readyInMinutes;
+    private String instructions;
+    private String summary;
+
+    public Recipe(int id, String title, String image, int servings, int healthScore, int cheap, int glutenFree, int dairyFree, int readyInMinutes, String instructions, String summary) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.servings = servings;
+        this.healthScore = healthScore;
+        this.cheap = cheap == 1;
+        this.glutenFree = glutenFree == 1;
+        this.dairyFree = dairyFree == 1;
+        this.readyInMinutes = readyInMinutes;
+        this.instructions = instructions;
+        this.summary = summary;
+    }
+}
