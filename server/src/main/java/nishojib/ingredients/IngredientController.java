8a1f0e04e8c121cb474e0d8f1f17010077693634
Saurabh Jain <saurabhjain@saurabhs-air.wireless.concordia.ca>
2022-11-13 21:59:04 -0500

    Added DELETE RESTful endpoint for Ingredients

--- server/src/main/java/nishojib/ingredients/IngredientController.java
+++ server/src/main/java/nishojib/ingredients/IngredientController.java
@@ -2,12 +2,18 @@ package nishojib.ingredients;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
 import nishojib.core.exceptions.DataMapperException;
+import nishojib.core.models.ErrorResult;
+import nishojib.core.models.Result;
+import nishojib.ingredients.models.DeletedIngredient;
 import nishojib.ingredients.models.Ingredient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.delete;
 
 public class IngredientController {
     IngredientDataMapper ingredientDataMapper;
@@ -45,6 +51,28 @@ public class IngredientController {
                 System.out.println(e.getStackTrace());
                 return "";
             }
+        });
+
+        delete("ingredients/:ingredientId", (req, res) -> {
+            res.type("application/json");
+            String ingredientIdStr = req.params(":ingredientId");
+            int ingredientId = Integer.parseInt(ingredientIdStr);
+            List<DeletedIngredient> ingredients = new ArrayList<>();
+
+            try {
+                boolean deleted = ingredientDataMapper.deleteById(ingredientId);
+                if (deleted) {
+                    DeletedIngredient ingredient = new DeletedIngredient(ingredientId);
+                    ingredients.add(ingredient);
+                } else {
+                    return gson.toJson(new ErrorResult("Unable to delete ingredient with id: " + ingredientId));
+                }
+            } catch (DataMapperException e) {
+                return gson.toJson(new ErrorResult(e.getMessage()));
+            }
+
+            Result<DeletedIngredient> result = new Result<>(ingredients);
+            return gson.toJson(result);
         });
 
     }
--- server/src/main/java/nishojib/ingredients/IngredientGateway.java
+++ server/src/main/java/nishojib/ingredients/IngredientGateway.java
@@ -28,10 +28,39 @@ public class IngredientGateway {
             String sql = "SELECT * FROM ingredients WHERE id=" + ingredientId;
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
-            conn.close();
             return rs;
         } catch (SQLException e) {
             throw new GatewayException("Error occurred reading ingredients from data source");
+        }
+    }
+
+    public void deleteById(int ingredientId) throws GatewayException {
+        Connection conn = null;
+
+        try {
+            conn = connect("recipes.sqlite");
+            conn.setAutoCommit(false);
+
+            String sqlForDeletingRecipeIngredients = "DELETE FROM recipes_ingredients WHERE ingredientId=" + ingredientId;
+            String sqlForDeletingIngredients = "DELETE FROM ingredients WHERE id=" + ingredientId;
+
+            PreparedStatement pstmt1 = conn.prepareStatement(sqlForDeletingRecipeIngredients);
+            pstmt1.executeUpdate();
+
+            PreparedStatement pstmt2 = conn.prepareStatement(sqlForDeletingIngredients);
+            pstmt2.executeUpdate();
+
+            conn.commit();
+
+            pstmt1.close();
+            pstmt2.close();
+        } catch (SQLException e1) {
+            try {
+                if (conn != null) conn.rollback();
+            } catch (SQLException e2) {
+                throw new GatewayException("Error rolling back.");
+            }
+            throw new GatewayException("Error occurred deleting ingredients from data source.");
         }
     }
 
